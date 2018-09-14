8e111b895dba5ec4ccbad297a3b7405147139f7d
Yaroslav Kosovych <bassicinbox@gmail.com>
2018-09-14 13:01:28 +0300

    finished hw_17

--- src/js/Carousel.js
+++ src/js/Carousel.js
@@ -8,10 +8,16 @@ function CarouselConstructor($container) {
   this.$container = document.querySelector($container);
   this.$carouselWrapper = this.$container.firstElementChild;
   this.$carouselWrapper.firstElementChild.append(this.$carouselWrapper.firstElementChild.children[0].cloneNode(true));
+
+  this.$carouselWrapper.firstElementChild.insertBefore(this.$carouselWrapper.firstElementChild.children[this.$carouselWrapper.firstElementChild.children.length - 2].cloneNode(true), this.$carouselWrapper.firstElementChild.children[0]);
+
   this.$slidesWrapper = this.$carouselWrapper.firstElementChild;
   this.$slides = Array.from(this.$slidesWrapper.children);
-  this.curentSlide = 0;
+  this.curentSlide = 1;
   this.$carouselWrapper.style = `max-width: ${(this.$container.offsetWidth - 40)}px`;
+
+  this.slidesWidth = this.$slides.map(el => el.offsetWidth);
+  console.log(this.slidesWidth);
 
   let self = this;
   let slidesW = 0;
@@ -21,10 +27,14 @@ function CarouselConstructor($container) {
     slidesW += el.offsetWidth;
   });
 
+  replaceSlider(null, 'start', this);
+  setTimeout(() => {
+    this.$slidesWrapper.style.transition = 'left 0.3s';
+  }, 0);
   this.$slidesWrapper.style.width = `${slidesW}px`;
 
   append('button', 'carousel-btn prev', this.$container).addEventListener('click', function (ev) {
-    self.nextSlide(ev, self);
+    self.prevSlide(ev, self);
   });
   append('button', 'carousel-btn next', this.$container).addEventListener('click', function (ev) {
     self.nextSlide(ev, self);
@@ -42,24 +52,59 @@ CarouselConstructor.prototype.nextSlide = function (ev, $this) {
   $this.curentSlide += 1;
 
   if (!$this.$slides[$this.curentSlide + 1]) {
-    $this.curentSlide = 0;
+    $this.curentSlide = 1;
     this.$slidesWrapper.addEventListener('transitionend', function as(ev, self) {
-      replaceSlider(ev);
+      ev.target.style.transition = 'none';
+      replaceSlider(ev, 'start', $this);
       ev.target.removeEventListener('transitionend', as);
+      setTimeout(() => {
+        ev.target.style.transition = ' left 0.3s'
+      }, 0);
     });
   }
 
   let shift = $this.$slides[$this.curentSlide].offsetWidth;
   console.log(shift);
-  
-  $this.$slidesWrapper.style.left = $this.$slidesWrapper.style.left === "" ?
-  `${-shift}px` :
-  `${parseInt($this.$slidesWrapper.style.left) - shift}px`;
-  
 
+  $this.$slidesWrapper.style.left = $this.$slidesWrapper.style.left === "" ?
+    `${-shift}px` :
+    `${parseInt($this.$slidesWrapper.style.left) - shift}px`;
 }
 
-function replaceSlider(ev, context) {
-  ev.target.style.left = 0;
-  console.log("asd");
+CarouselConstructor.prototype.prevSlide = function (ev, $this) {
+  $this.curentSlide -= 1;
+
+  if (!$this.$slides[$this.curentSlide - 1]) {
+    $this.curentSlide = $this.$slides.length - 2;
+    this.$slidesWrapper.addEventListener('transitionend', function ass(ev, self) {
+      ev.target.style.transition = 'none';
+      replaceSlider(ev, 'end', $this);
+      ev.target.removeEventListener('transitionend', ass);
+
+      setTimeout(() => {
+        ev.target.style.transition = ' left 0.3s'
+      }, 0);
+    });
+  }
+
+  let shift = $this.$slides[$this.curentSlide].offsetWidth;
+  console.log(shift);
+
+  $this.$slidesWrapper.style.left = $this.$slidesWrapper.style.left === "" ?
+    `${shift}px` :
+    `${parseInt($this.$slidesWrapper.style.left) + shift}px`;
+}
+
+
+
+function replaceSlider(ev, controller, context) {
+  if (controller === 'start') {
+    context.$slidesWrapper.style.left = `-${context.slidesWidth[0]}px`;
+  }
+
+  if (controller === 'end') {
+    context.$slidesWrapper.style.left = '-' + context
+      .slidesWidth.slice(0, context.slidesWidth.length - 2)
+      .reduce((accumulator, currentValue) => accumulator + currentValue) + 'px';
+  }
 }
