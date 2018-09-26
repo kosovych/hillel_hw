d99ff66b34e173399e0b094e278338c5052fd519
Yaroslav Kosovych <bassicinbox@gmail.com>
2018-09-26 12:56:28 +0300

    windowSizeBadge fix issue

--- src/js/windowSizeBadge.js
+++ src/js/windowSizeBadge.js
@@ -1,14 +1,17 @@
+module.exports = windowSizeBadge;
+
+const resiseObj = {
+  curentSetTimeoutId: undefined
+}
+
 function windowSizeBadge($badge) {
-  $badge.setAttribute('style', '');
   if(resiseObj.curentSetTimeoutId) {
     clearInterval(resiseObj.curentSetTimeoutId);
   }
-   $badge.innerText = `${window.innerWidth}px \u00d7 ${window.innerHeight}px`;
   
   resiseObj.curentSetTimeoutId = setTimeout(() => {
-    $badge.setAttribute('style', 'opacity: 0');
     $badge.innerText = `${window.innerWidth}px \u00d7 ${window.innerHeight}px`;
     $badge.setAttribute('style', 'opacity: 1');
     setTimeout(()=>{$badge.setAttribute('style', 'opacity: 0');}, 2000)
   }, 2000);
-} 
+}
