5148803789f1025ee56a4ab18ad3fdfa3a7876ea
Yaroslav Kosovych <bassicinbox@gmail.com>
2018-09-26 12:39:32 +0300

    windowSizeBadge fix

--- src/js/windowSizeBadge.js
+++ src/js/windowSizeBadge.js
@@ -5,13 +5,13 @@ const resiseObj = {
 }
 
 function windowSizeBadge($badge) {
-  $badge.setAttribute('style', '');
   if(resiseObj.curentSetTimeoutId) {
     clearInterval(resiseObj.curentSetTimeoutId);
   }
-
-  $badge.innerText = `${window.innerWidth}px \u00d7 ${window.innerHeight}px`;
+  
   resiseObj.curentSetTimeoutId = setTimeout(() => {
-    $badge.setAttribute('style', 'opacity: 0');
+    $badge.innerText = `${window.innerWidth}px \u00d7 ${window.innerHeight}px`;
+    $badge.setAttribute('style', 'opacity: 1');
+    setTimeout(()=>{$badge.setAttribute('style', 'opacity: 0');}, 2000)
   }, 2000);
 }
